449e470a5b56c89b0812a895c962e23b72687349
Alexandre Boucey <alexandre.boucey@gmail.com>
2014-07-09 21:46:10 +0200

    save/restore

--- js/LolCalc.js
+++ js/LolCalc.js
@@ -1,7 +1,11 @@
+"use strict";
+
 var LolCalc = new function () {
-	var builds = {};
+	var builds = [];
 	var currentBuildIndex = 0;
 	builds[0] = new Build();
+
+
 
 
 	this.getBuild = function (n){
@@ -12,46 +16,74 @@ var LolCalc = new function () {
 	this.getBuildCount = function (){
 		return builds.length
 	}
+
+	this.save = function () {
+		var buildsData = []
+		for (var i = builds.length - 1; i >= 0; i--) {
+			buildsData[i] = builds[i].save();
+		};
+		localStorage.setItem('LolCalc', JSON.stringify(buildsData));
+		console.log(buildsData);
+	}
+
+	this.load = function (callback) {
+		if(localStorage.getItem('LolCalc')===null)
+			return console.log('no datas');
+		var buildsData = JSON.parse(localStorage.getItem('LolCalc'));
+		builds = [];
+		currentBuildIndex = 0;
+		for (var i = buildsData.length - 1; i >= 0; i--) {
+			builds[i] = new Build(buildsData[i]);
+		};
+		if('function'===typeof callback)
+			callback();
+	}
 }
 
-function RuneSet (parent) {
+function RuneSet (parent, runeSet) {
 	var _parent = parent;
-	var runeSet = {
+	if('undefined'===typeof runeSet)
+		runeSet = {
 		blue: [],
 		red: [],
 		yellow: [],
 		black: []
-	}
+	};
+	var _runeSet = runeSet;
 
 	this.getRuneList = function (type) {
-		return runeSet[type];
+		return _runeSet[type];
 	}
 	this.getAllRunes = function () {
 		var r = [];
-		for(var runeType in runeSet){
-			r = r.concat(runeSet[runeType]);
+		for(var runeType in _runeSet){
+			r = r.concat(_runeSet[runeType]);
 		}
 		return r;
 	}
 	this.appendRune = function (runeId) {
 		var rune = DataLoader.rune.data[runeId];
-		if (runeSet[rune.rune.type].length >= RuneSet.RUNE_COUNT[rune.rune.type])
+		if (_runeSet[rune.rune.type].length >= RuneSet.RUNE_COUNT[rune.rune.type])
 			return -1;
-		console.log("Adding " + rune.name + " (" + runeId + ") @ " + runeSet[rune.rune.type].length);
-		runeSet[rune.rune.type].push(runeId);
-		runeSet[rune.rune.type].sort();
+		console.log("Adding " + rune.name + " (" + runeId + ") @ " + _runeSet[rune.rune.type].length);
+		_runeSet[rune.rune.type].push(runeId);
+		_runeSet[rune.rune.type].sort();
 		_parent.compile();
-		return runeSet[rune.rune.type].length-1;
+		return _runeSet[rune.rune.type].length-1;
 	}
 	this.removeRune = function (runeId) {
 		var rune = DataLoader.rune.data[runeId];
-		var pos = runeSet[rune.rune.type].indexOf(runeId);
+		var pos = _runeSet[rune.rune.type].indexOf(runeId);
 		if (pos == -1)
 			return -1;
 		console.log("Removing " + rune.name + " (" + runeId + ") @ " + pos);
-		runeSet[rune.rune.type].splice(pos, 1);
+		_runeSet[rune.rune.type].splice(pos, 1);
 		_parent.compile();
-		return runeSet[rune.rune.type].length-1;
+		return _runeSet[rune.rune.type].length-1;
+	}
+
+	this.save = function () {
+		return _runeSet;
 	}
 }
 RuneSet.RUNE_TYPE = {
@@ -77,18 +109,27 @@ function Masteries () {
 
 function Champion () {
 	this.key = null;
-	this.level = 1;
 }
 
 
 
-function Build () {
-	this.champion = new Champion(this);
-	this.masteries = new Masteries(this);
-	this.runes = new RuneSet(this);
-	this.items = new ItemSet(this);
+function Build (d) {
+	if('undefined'===typeof d)
+		d = {
+			champion: undefined,
+			masteries: undefined,
+			runes: undefined,
+			items: undefined,
+			name: ''
+		}
+	this.champion = new Champion(this, d.champion);
+	this.masteries = new Masteries(this, d.masteries);
+	this.runes = new RuneSet(this, d.runes);
+	this.items = new ItemSet(this, d.items);
 
 	this.compiledStats = null;
+
+	this.name = d.name;
 }
 Build.prototype.compile = function() {
 	var runeStats = jQuery.extend({}, DataLoader.rune.basic.stats)
@@ -105,4 +146,10 @@ Build.prototype.compile = function() {
 	}
 	this.compiledStats = runeStats;
 	return runeStats;
+};
+
+Build.prototype.save = function() {
+	return {
+		runes: this.runes.save(),
+	}
 };
